c386f6f8724cfc83b584aaddf5ad37295ae1b744
rwdtdev <rwdtdev@gmail.com>
2025-08-11 11:23:02 +0300

    move calendar to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,36 @@
-import dayjs from 'dayjs';
-import isoWeek from 'dayjs/plugin/isoWeek';
-import 'dayjs/locale/ru'; // Importing Russian locale for dayjs
-import Header from '@/components/Header';
+import { CalendarsBlock2 } from '@/components/CalendarsBlock2';
 import ResultBlock from '@/components/ResultBlock';
-import CalendarsBlock from '@/components/CalendarsBlock';
-import ClientContainerVH from '@/components/ClientContainerVH';
+import { createCalendarJson } from '@/lib/createCalendarJson';
+import { createDaysArr } from '@/lib/createDaysArr';
 import { createYearCalendar } from '@/lib/createYearCalendar';
+import dayjs from 'dayjs';
+import { promises as fs } from 'fs';
 
-dayjs.locale('ru');
-dayjs.extend(isoWeek);
-// Importing dayjs with isoWeek plugin for week calculations
-// Importing Russian locale for dayjs
+export type Day = {
+  dateString: string;
+  isHoliday: boolean;
+  isWeekend: boolean;
+  dayOfYear: number;
+  year: number;
+};
 
-export default async function HomePage() {
-  const months = await createYearCalendar({
-    year: dayjs().year(),
-  });
+export default async function C2Page() {
+  // createDaysArr({ year: 2025 }).then((res) => {
+  //   console.log('🚀 ~ C2Page ~ res:', new Date(), res);
+  //   fs.writeFile('src/constant/calendars2.json', JSON.stringify(res));
+  // });
+
+  const file = await fs.readFile(
+    process.cwd() + '/src/constant/calendars2.json',
+    'utf8',
+  );
+  const days: Day[] = JSON.parse(file);
+  console.log('🚀 ~ C2Page ');
+
   return (
-    // <ClientContainerVH>
-    //   <Header />
-    // <div className='flex flex-col overflow-y-hidden border-2 border-blue-500 xl:flex-row'>
     <div className='/border-2 /border-blue-500 flex flex-col overflow-y-hidden xl:flex-row'>
-      <CalendarsBlock months={months} />
-      <ResultBlock />
+      <CalendarsBlock2 days={days} />
+      <ResultBlock days={days} />
     </div>
-    // </div>
-    // </ClientContainerVH>
   );
 }
